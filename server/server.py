cc6f9d66ec77aaa2e363a61fcc964abbdeb939b9
denzi <40885243+dennzii@users.noreply.github.com>
2026-09-06 17:51:19 +0300

    api exceptions is being implemented

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 import numpy as np
 import onnxruntime as ort
 import torch
@@ -6,7 +6,7 @@ from TabPFNTransformer import Transformer_TabPFN
 from predict import predict
 from TabPFN_Request import TabPFN_Request
 
-
+MAX_TRAIN_SAMPLES = 800
 app = FastAPI(title='TabPFN v1 Inference Server')
 
 
@@ -23,7 +23,11 @@ def predict_(request : TabPFN_Request):
     y_train =  np.array(request.y_train,dtype=np.float32)
     X_test =  np.array(request.X_test,dtype=np.float32)
 
-
+    if X_train.shape[0] > MAX_TRAIN_SAMPLES:
+        raise HTTPException(400,f'Train sample size cannot exceed {MAX_TRAIN_SAMPLES}. Be Sure your train subsample stratified accoording to target labels')
+    elif len(X_train) == 0 or len(y_train) or len(X_test):
+        raise HTTPException(400,f'any of X_train, y_train, X_test cannot be empty!')
+    
     out,probs = predict(model= model,device= device,X_train=X_train,y_train=y_train,X_test=X_test)
 
 
